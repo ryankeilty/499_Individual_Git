d9c65a212dd77130132a4ba8b8286c19c64c1dae
ryankeilty <rkeilty19@gmail.com>
2021-09-16 20:47:53 -0700

    Added input reading functionality

--- Sorter/src/Sorter.java
+++ Sorter/src/Sorter.java
@@ -1,8 +1,25 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class Sorter{
 
     public static void main(String[]args){
+        Scanner read = new Scanner(System.in);
 
-        System.out.println("hello");
+        Boolean correct = false;
+        int inType = 0;
+
+        System.out.println("Hello, please indicate 1 for numbers and 2 for words\n");
+
+        while(!correct) {
+           try{
+               inType = read.nextInt();
+               correct = true;
+           } catch(InputMismatchException e) {
+               System.out.println("Please enter 1 for numbers or 2 for words");
+               read.next();
+           }
+        }
+        
     }
 
 }
